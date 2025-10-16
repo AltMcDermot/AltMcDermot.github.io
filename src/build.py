9e0d74e80e2e258233a762c1ababdcc0579d9315
melto <marink009@hotmail.com>
2025-10-16 14:56:31 -0400

    updating comand

--- src/build.py
+++ src/build.py
@@ -1,22 +1,21 @@
 import os
 import shutil
 from block_markdown import markdown_to_html_node
-
+# from main import basepath
 
 def buildSite():
     print("starting build...")
     path = "./public/"
 
-    if os.path.exists(path):
-        shutil.rmtree(path+'')
-    os.makedirs(path)
+    # if os.path.exists(path):
+    #     shutil.rmtree(path+'')
+    # os.makedirs(path)
     copy_static_files("./static/", path)
 
-    generate_page("./content/", "./template.html", "./public/")
     
-    print("build complete")
+    
 
-def copy_static_files(src,dest):
+def copy_static_files(src, dest):
     # print("=====Copy==Start=====")
     if not src.endswith("/"):
         src=src+"/"
@@ -43,7 +42,7 @@ def extract_title(markdown):
             header = line[2:]
     return header
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     if not from_path.endswith("/"):
         from_path=from_path+"/"
     if not dest_path.endswith("/"):
@@ -53,19 +52,25 @@ def generate_page(from_path, template_path, dest_path):
         for item in os.listdir(from_path):
             if os.path.isdir(from_path+item):
                 os.makedirs(dest_path+item, exist_ok=True)
-                generate_page(from_path+item+'/', template_path, dest_path+item)
+                generate_page(from_path+item+'/', template_path, dest_path+item, basepath)
 
             else:
                 print(f"generating page from {item} to {dest_path} using {template_path}")
 
                 header = extract_title(from_path+item)
                 markdownfile = open(from_path+item, 'r')
-                lines = markdownfile.read()
+                markdown_lines = markdownfile.read()
 
                 template_file = open(template_path, 'r')
                 template_lines = template_file.readlines()
+                html = "" 
         
-                nodes = markdown_to_html_node(lines)
+                nodes = markdown_to_html_node(markdown_lines)
+                # for node in markdown_lines:
+                #     new_node = markdown_to_html_node(node)
+                #     print(node)
+                #     print(new_node.to_html())
+                #     html = html + new_node.to_html() 
                 markup = ""
                 html = nodes.to_html()
 
@@ -74,6 +79,12 @@ def generate_page(from_path, template_path, dest_path):
                         line = line.replace("{{ Content }}", html)
                     if line.find("{{ Title }}") != -1:
                         line = line.replace("{{ Title }}", header.strip())
+                    if line.find('href="/') != -1:
+                        # print(line.find("href=\"/"))
+                        line = line.replace('href="/', 'href="'+basepath)
+                    if line.find('src="/') != -1:
+                        print("match")
+                        line = line.replace('src="/', 'src="'+basepath)
                     markup += line
 
 
--- src/main.py
+++ src/main.py
@@ -1,9 +1,31 @@
+import sys
+import os
+import shutil
+
 from textnode import *
 from htmlnode import *
 from inline_markdown import *
-from build import *
+from build import copy_static_files, generate_page
+
+dir_path_static = "./static"
+dir_path_public = "./public"
+dir_path_content = "./content"
+template_path = "./template.html"
+basepath = "/"
 
 def main():
-    buildSite()
+    if len(sys.argv) > 1:
+        basepath = sys.argv[1]
+
+    if os.path.exists(dir_path_public):
+        shutil.rmtree(dir_path_public+'')
+    os.makedirs(dir_path_public)
+
+    print("starting build...")
+
+    copy_static_files(dir_path_static, dir_path_public)
+    generate_page(dir_path_content, template_path, dir_path_public, basepath)
+
+    print("build complete")
 
 main()
